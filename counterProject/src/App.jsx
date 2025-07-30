dc26c3c0218bd612246f9b465d5e4a39f455d98f
Rudra2125 <rudra25parmeshwar@gmail.com>
2025-07-30 23:44:16 +0530

    react setup and use state

---
+++ counterProject/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // let counter = 5
+  
+  const [counter, setCounter] = useState(0)
+
+  const addValue =()=>{
+    // counter = counter+1;
+    setCounter(counter+1)
+    console.log("Value added", counter);
+  }
+  const decreaseValue =()=>{
+    setCounter(counter-1)
+    console.log("Value Decresed", counter);
+
+  }
+  return (
+    <>
+      <h1 className='mb-5'>Hello </h1>
+      <h2 className='mb-5'>Counter Value : {counter}</h2>
+
+      <div className='flex gap-5'>
+      <button onClick={addValue}>Increase</button>
+      <button onClick={decreaseValue}>Decrease</button>
+      </div>
+    </>
+  )
+}
+
+export default App
