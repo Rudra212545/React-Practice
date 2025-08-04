c3e66abc88ad43e93190f69ff811c806d58364c1
Rudra2125 <rudra25parmeshwar@gmail.com>
2025-08-04 23:50:04 +0530

    react router dom half completed

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
+import Footer from "./components/footer/Footer"
+
+function App() {
+  
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ reactRouter/src/components/User/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+  const {userId} = useParams()
+  return (
+    <div className='text-center bg-gray-400 text-white p-4 text-3xl font-bold'>
+      User: {userId}
+    </div>
+  )
+}
+
+export default User
