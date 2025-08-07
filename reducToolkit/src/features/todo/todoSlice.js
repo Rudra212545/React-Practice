2e08b413a3cb5778d151d01992db5d404bff2d2e
Rudra2125 <rudra25parmeshwar@gmail.com>
2025-08-07 23:59:03 +0530

    add reducers

--- reducToolkit/src/features/todo/todoSlice.js
+++ reducToolkit/src/features/todo/todoSlice.js
@@ -1,18 +1,36 @@
-import {createSlice, nanoid} from "reducjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 
-const intialState = {
+
+const initialState = {
     todos:[{id:1,text:"Hello World"}]
 }
 
-const 
+
 
 export const todoSlice = createSlice({
     name:"todo",
-    intialState,
+    initialState,
     reducers:{
-        addTodo:(state,action )=>{},
-        removeTodo:()=>{},
+        addTodo:(state,action)=>{
+            const todo ={
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{    
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload;
+            state.todos = state.todos.map(todo =>
+              todo.id === id
+                ? { ...todo, text }
+                : todo
+            );
+          }
+          
     }
 
 })
